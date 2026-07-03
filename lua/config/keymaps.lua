824b42e2480db9911f1e0b7d5545094e02373f55
victorsidedev <victorside.dev@gmail.com>
2026-07-03 09:35:03 -0500

    new keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -24,3 +24,7 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- keep visual selection after applying custom indentation
+vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
